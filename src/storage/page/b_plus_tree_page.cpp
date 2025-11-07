160618a7a2fd69a9316bf8dcc13d718c096b9203
aleeeeeexxxx <hanzezhenalex@gmail.com>
2025-11-07 07:49:54 +0000

    fix: format

--- src/storage/page/b_plus_tree_page.cpp
+++ src/storage/page/b_plus_tree_page.cpp
@@ -42,9 +42,7 @@ void BPlusTreePage::SetMaxSize(int size) { max_size_ = size; }
  * Generally, min page size == max page size / 2
  * But whether you will take ceil() or floor() depends on your implementation
  */
-auto BPlusTreePage::GetMinSize() const -> int {
-  return std::ceil(static_cast<double>(GetMaxSize()) / 2);
-}
+auto BPlusTreePage::GetMinSize() const -> int { return std::ceil(static_cast<double>(GetMaxSize()) / 2); }
 
 auto BPlusTreePage::CanReleaseAncestor(bool insert) const -> bool {
   if (insert) {
